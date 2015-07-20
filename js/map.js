8cb3507ec6cade7e4e9b0124805e0910d34b670a
jimjonesbr <jimjonesbr@gmail.com>
2015-07-20 18:48:55 +0200

    Fature for drawing spatial filter polygons implemented.

--- js/map.js
+++ js/map.js
@@ -1,4 +1,6 @@
 var tmpBBOX = 0;
+var featureGroup;
+var mapsGroup;
 
 function init() {
 
@@ -6,12 +8,54 @@ function init() {
     L.mapbox.accessToken = 'pk.eyJ1Ijoiamltam9uZXMiLCJhIjoib2R0ZUVmTSJ9.9fXpF8LWx9bm2WSW6hg4PQ';	
 
 	LMap = L.mapbox.map('map', 'examples.map-i86nkdio', {attributionControl: true}).setView([52, 5], 3);
-	markerGroup = new L.featureGroup();
-	markerGroup.addTo(LMap);
-	//markerGroup.addLayer(omnivore.wkt.parse('POINT(-90 52)'));
-	//LMap.fitBounds(markerGroup.getBounds());
+	mapsGroup = new L.featureGroup();
+	mapsGroup.addTo(LMap);
+	 
+    featureGroup = new L.featureGroup().addTo(LMap);
+
+	var drawControl = new L.Control.Draw({
+	    edit: {
+	      featureGroup: featureGroup
+	    },
+  		draw: {
+		    polygon: true,
+		    polyline: false,
+		    rectangle: true,
+		    circle: false,
+		    marker: false
+
+  		}
+  	}).addTo(LMap);
 
 
+	//** Event triggered when a geometry is created.
+	LMap.on('draw:created', function(e) {
+   
+	  featureGroup.clearLayers();
+	  mapsGroup.clearLayers();
+
+      featureGroup.addLayer(e.layer);
+      console.log(toWKT(e.layer));
+      
+      wktBBOX=toWKT(e.layer);
+      
+	  executeQuery(0);
+
+	  //** Removing (if applicable) spatial filter to current view.
+	  LMap.removeLayer(tmpBBOX);
+	  wktBBOX="";
+
+	  $("#selectArea").addClass('').removeClass('active');
+	  $("#selectArea").attr('value', 'remove');
+	  $("#selectArea").html('Remove Spatial Filter');
+
+  	});
+
+	//mapsGroup.addLayer(omnivore.wkt.parse('POINT(-90 52)'));
+	//LMap.fitBounds(mapsGroup.getBounds());
+
+
+/*
     var info = L.control();
 
 	info.onAdd = function (map) {
@@ -19,7 +63,7 @@ function init() {
 	    this.update();
 	    return this._div;
 	};
-
+*/
 	// method that we will use to update the control based on feature properties passed
 	
 
@@ -50,12 +94,12 @@ function init() {
 function plotGeometry(wkt) {
 
 
-	markerGroup.clearLayers();
-	markerGroup.addLayer(omnivore.wkt.parse(unescape(wkt)));
+	mapsGroup.clearLayers();
+	mapsGroup.addLayer(omnivore.wkt.parse(unescape(wkt)));
 
 	if($("#automaticZoom").attr("value")=="on") 
 	{
-	  LMap.fitBounds(markerGroup.getBounds());
+	  LMap.fitBounds(mapsGroup.getBounds());
 	}
 	
 }
@@ -66,12 +110,17 @@ function setSpatiatConstraint(){
 	$('#btnSpatialConstraint').prop('disabled', true);
 	$('#btnRemoveSpatialConstraint').prop('disabled', false);
 
+	//** Removes any previously drawn filter geometry.
+	featureGroup.clearLayers();
+
 	//** Creates a rectangle based on the current BBOX
 	var bounds = [[LMap.getBounds().getNorth(), LMap.getBounds().getEast()], [LMap.getBounds().getSouth(), LMap.getBounds().getWest()]];
 	tmpBBOX = L.rectangle(bounds, {color: "#ff7800", weight: 1});
 
 	tmpBBOX.addTo(LMap);
-	
+
+
+
 	//** Setting spatial constraint and executing query to filter out records outside the given BBOX.
 	wktBBOX=toWKT(tmpBBOX);
 	executeQuery(0);
@@ -87,8 +136,13 @@ function removeSpatiatConstraint(){
 	LMap.removeLayer(tmpBBOX);
 	wktBBOX="";
 	
+	//** Removing (if applicable) filter geometry.
+	featureGroup.clearLayers();
+	
 	//** Removing spatial constraint from the result panel.
 	executeQuery(0);
+
+
 }
 
 
